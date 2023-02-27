24e7ae1692c53d6a11c9034491d9088f03103c2e
Charlie Saleh <charliesaleh@outlook.com>
2023-02-27 07:16:25 -0500

    week 4 project

---
+++ car_dealership_dml.sql
@@ -0,0 +1,324 @@
+INSERT INTO customer(
+    first_name,
+    last_name,
+    email,
+    phone_number
+    ) VALUES (
+
+    );
+
+INSERT INTO customer(
+    first_name,
+    last_name,
+    email,
+    phone_number
+    ) VALUES (
+        
+    );
+
+INSERT INTO customer(
+    first_name,
+    last_name,
+    email,
+    phone_number
+    ) VALUES (
+        
+    );
+
+INSERT INTO customer(
+    first_name,
+    last_name,
+    email,
+    phone_number
+    ) VALUES (
+        
+    );
+
+INSERT INTO customer(
+    first_name,
+    last_name,
+    email,
+    phone_number
+    ) VALUES (
+        
+    );
+
+INSERT INTO invoice(
+    amount,
+    payment_date
+    ) VALUES (
+
+    );
+
+INSERT INTO invoice(
+    amount,
+    payment_date
+    ) VALUES (
+        
+    );
+
+INSERT INTO invoice(
+    amount,
+    payment_date
+    ) VALUES (
+        
+    );
+
+INSERT INTO invoice(
+    amount,
+    payment_date
+    ) VALUES (
+        
+    );
+
+INSERT INTO invoice(
+    amount,
+    payment_date
+    ) VALUES (
+        
+    );
+
+INSERT INTO inventory(
+    new_car,
+    amount,
+    quantity
+) VALUES (
+
+);
+
+INSERT INTO inventory(
+    new_car,
+    amount,
+    quantity
+) VALUES (
+
+);
+
+INSERT INTO inventory(
+    new_car,
+    amount,
+    quantity
+) VALUES (
+
+);
+
+INSERT INTO inventory(
+    new_car,
+    amount,
+    quantity
+) VALUES (
+
+);
+
+INSERT INTO inventory(
+    new_car,
+    amount,
+    quantity
+) VALUES (
+
+);
+
+INSERT INTO car(
+    car_make,
+    car_model,
+    car_year,
+    serial_number,
+    license_plate,
+    mileage
+) VALUES (
+
+);
+
+INSERT INTO car(
+    car_make,
+    car_model,
+    car_year,
+    serial_number,
+    license_plate,
+    mileage
+) VALUES (
+    
+);
+
+INSERT INTO car(
+    car_make,
+    car_model,
+    car_year,
+    serial_number,
+    license_plate,
+    mileage
+) VALUES (
+    
+);
+
+INSERT INTO car(
+    car_make,
+    car_model,
+    car_year,
+    serial_number,
+    license_plate,
+    mileage
+) VALUES (
+    
+);
+
+INSERT INTO car(
+    car_make,
+    car_model,
+    car_year,
+    serial_number,
+    license_plate,
+    mileage
+) VALUES (
+    
+);
+
+INSERT INTO salesperson(
+    first_name,
+    last_name,
+    email
+) VALUES (
+
+);
+
+INSERT INTO salesperson(
+    first_name,
+    last_name,
+    email
+) VALUES (
+
+);
+
+INSERT INTO salesperson(
+    first_name,
+    last_name,
+    email
+) VALUES (
+
+);
+
+INSERT INTO salesperson(
+    first_name,
+    last_name,
+    email
+) VALUES (
+
+);
+
+INSERT INTO salesperson(
+    first_name,
+    last_name,
+    email
+) VALUES (
+
+);
+
+INSERT INTO cars_serviced(
+    quantity
+) VALUES (
+
+);
+
+INSERT INTO cars_serviced(
+    quantity
+) VALUES (
+
+);
+
+INSERT INTO cars_serviced(
+    quantity
+) VALUES (
+
+);
+
+INSERT INTO cars_serviced(
+    quantity
+) VALUES (
+
+);
+
+INSERT INTO cars_serviced(
+    quantity
+) VALUES (
+
+);
+
+INSERT INTO service_tickets(
+    service_date,
+    amount,
+    car_description
+) VALUES (
+
+);
+
+INSERT INTO service_tickets(
+    service_date,
+    amount,
+    car_description
+) VALUES (
+
+);
+
+INSERT INTO service_tickets(
+    service_date,
+    amount,
+    car_description
+) VALUES (
+
+);
+
+INSERT INTO service_tickets(
+    service_date,
+    amount,
+    car_description
+) VALUES (
+
+);
+
+INSERT INTO service_tickets(
+    service_date,
+    amount,
+    car_description
+) VALUES (
+
+);
+
+INSERT INTO mechanic(
+    first_name,
+    last_name,
+    email
+) VALUES (
+
+);
+
+INSERT INTO mechanic(
+    first_name,
+    last_name,
+    email
+) VALUES (
+
+);
+
+INSERT INTO mechanic(
+    first_name,
+    last_name,
+    email
+) VALUES (
+
+);
+
+INSERT INTO mechanic(
+    first_name,
+    last_name,
+    email
+) VALUES (
+
+);
+
+INSERT INTO mechanic(
+    first_name,
+    last_name,
+    email
+) VALUES (
+
+);
